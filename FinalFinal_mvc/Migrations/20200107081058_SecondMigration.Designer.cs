2504e9817eb48aeddfbd51cf468c295430fa326c
YoonkunLee <ydx9474@aut.ac.nz>
2020-01-07 21:18:36 +1300

    Connect database in new computer

--- FinalFinal_mvc/Migrations/20200107081058_SecondMigration.Designer.cs
+++ FinalFinal_mvc/Migrations/20200107081058_SecondMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FinalFinal_mvc.Migrations
 {
     [DbContext(typeof(CustomerDbContext))]
-    partial class CustomerDbModelSnapshot : ModelSnapshot
+    [Migration("20200107081058_SecondMigration")]
+    partial class SecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
