11188547e2c8587d49081edbad7cb349fb6915e5
YoonkunLee <ydx9474@aut.ac.nz>
2020-01-14 00:51:15 +1300

    Update gitignore / fail to install SignalR

--- FinalFinal_mvc/Startup.cs
+++ FinalFinal_mvc/Startup.cs
@@ -27,6 +27,7 @@ namespace FinalFinal_mvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
